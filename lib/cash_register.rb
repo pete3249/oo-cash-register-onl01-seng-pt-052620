60039a6ca371f73fef61dea3a1a11ee59ac1e82c
Katelyn Peterson <katelynrpeterson16@gmail.com>
2020-06-18 21:15:07 +0000

    Automatically backed up by Learn

--- lib/cash_register.rb
+++ lib/cash_register.rb
@@ -1,1 +1,39 @@
+require 'pry'
+
+class CashRegister
+  attr_accessor :total, :discount, :items
+  
+  def initialize(discount = 0)
+    @total = 0
+    @discount = discount
+  end 
+  
+  def total
+    @total
+  end 
+  
+  def add_item(title, price, quantity = 1)
+    @items = []
+    sum = price * quantity
+    @total = @total + sum
+    @items << title * quantity
+    @items.flatten
+  end 
+  
+  def apply_discount
+    if @discount == 0
+      "There is no discount to apply."
+    else
+      discount_amount = @total/5
+      @total = @total - discount_amount
+      "After the discount, the total comes to $#{@total}."
+    end 
+  end 
+  
+  def items
+    @items
+  end 
+
+end 
+
 
